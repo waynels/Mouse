cfe27f761d3fb0f0868666568ce2289bc764b893
wanghui <wanghui@email.com>
2015-05-18 15:24:33 +0800

    小鼠创建”

--- db/migrate/20150514080409_create_operations.rb
+++ db/migrate/20150514080409_create_operations.rb
@@ -7,7 +7,7 @@ class CreateOperations < ActiveRecord::Migration
       t.integer :old_basket
       t.integer :new_basket
       t.integer :operate_by
-      t.integer :operate_at
+      t.date :operate_at
       t.text :remark
 
       t.timestamps null: false
