2d6f81c0c67bd912ab9c7bec71546cef3a97577a
wanghui <wanghui@email.com>
2015-07-13 15:42:32 +0800

    color

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,6 +1,7 @@
 class Strain < ActiveRecord::Base
   has_many :mice
   belongs_to :genetic_type
+  has_many :genes
   has_and_belongs_to_many :genes, :join_table => "strains_genes", :foreign_key => "strain_id"
   STATUS = {"Active" => "A", "Discarded" => "D", "Frozen" => "F"}
   def status_lable 
